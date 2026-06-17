ecff6b218034a99e5c64a0175434a32ec7846c17
Leclerc Christopher <leclerc.christopher@hotmail.com>
2026-06-17 12:31:02 +0200

    Feat: (html + css + js) Ajout de plusieurs fonctionnalités
    HTML
    index:
    * Ajout du "mode édition" lors de la connexion de l'utilisateur
    * Ajout de bouton "modifier" à coté du titre projet + apparition et disparition si token est présent.
    * Ajout de la page "Galerie photo" avec les fonctionnalités suivantes:
    - Bouton close
    - Bouton "ajouter une photo"
    * Ajout de la page "Ajout photo" avec les fonctionnalités suivantes:
    - Bouton close
    - Bouton return
    
    CSS:
    index:
    * Réajustement de certain éléments
    * Décoration de la page "Galerie photo" et "Ajout photo"
    * Création du fichier css pour une version mobile
    
    login:
    * Réajustement de certain éléments
    
    JS
    indexModuleAndSystem:
    * Ajout de plusieurs éléments tel que:
    - Fonctionnement des boutons
    - Affichage des menus

--- FrontEnd/indexModuleAndSystem.js
+++ FrontEnd/indexModuleAndSystem.js
@@ -11,14 +11,48 @@ function isUserLoggedIn() {
 // Utiliser cette fonction
 if (isUserLoggedIn()) {
     console.log('✅ L\'utilisateur est connecté');
+    let editorOn = document.getElementById("editorOn");
+    editorOn.style.display = "block";
     let login = document.getElementById("login");
     let logout = document.getElementById("logout");
     let logoutUserBtn = document.getElementById("logoutUser");
     logoutUserBtn.addEventListener("click", logoutUser);
     login.style.display = "none";
     logout.style.display = "block";
-
-
+    let boxButtonFiltre = document.getElementById("boxButtonFiltre");
+    boxButtonFiltre.style.display = "none";
+    const modifyButton = document.getElementById("modifyButton");
+    modifyButton.style.display = "block";
+    const modifyPage = document.getElementById("modifyPage");
+    const closeModifyPage = document.getElementById("closeModifyPage");
+    modifyButton.addEventListener("click", () => {
+        modifyPage.style.display = "block";
+    });
+    closeModifyPage.addEventListener("click", () => {
+        modifyPage.style.display = "none";
+    });
+    // Fermer en cliquant sur le fond
+    closeModifyPage.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            modal.classList.remove('active');
+        }
+    });
+    //addPicture
+    const addPicture = document.getElementById("addPicture");
+    const addPictureButton = document .getElementById("addPictureButton");
+    addPictureButton.addEventListener("click", () => {
+        addPicture.style.display = "block";
+        modifyPage.style.display = "none";
+    });
+    const closeAddPicture = document.getElementById("closeAddPicture");
+    closeAddPicture.addEventListener("click", () => {
+        addPicture.style.display = "none";
+    });
+    const returnAddPicture = document.getElementById("returnAddPicture");
+    returnAddPicture.addEventListener("click", () => {
+        addPicture.style.display = "none";
+        modifyPage.style.display = "block";
+    });
 } else {
     console.log('❌ L\'utilisateur n\'est pas connecté');
 }
@@ -29,4 +63,3 @@ function logoutUser() {
     window.location.href = "./index.html";
     console.log('User logged out');
 }
-
